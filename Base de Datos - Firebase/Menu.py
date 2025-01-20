bb6d5baafef3648bd52a7acdea2040b7a6f026db
ldanielamb <lamarinb@unal.edu.co>
2025-01-20 12:46:28 -0500

    correo ajustado

--- Base de Datos - Firebase/Menu.py
+++ Base de Datos - Firebase/Menu.py
@@ -1,8 +1,14 @@
 import firebase_admin
 from firebase_admin import credentials, db
+import re 
+"""
+Esta librería va a permitir: Buscar coincidencias en cadenas, validar texto, reemplazar texto,
+dividir texto en partes y extraer texto de una cadena. Es especialmente útil al momento de
+manejar los errores de escritura por parte del usuario :)
+"""
 credentials
 #Conectar a firebase
-cred = firebase_admin.credentials.Certificate(r"C:\Users\User\Documents\Visual Studio Code - Programación\Python\Firebase\Firebase compartido - Batalla naval\bookstoreproject-8b4f0-firebase-adminsdk-2eymv-b7972991ba.json")
+cred = firebase_admin.credentials.Certificate(r"bookstoreproject-8b4f0-firebase-adminsdk-2eymv-b7972991ba.json")
 default_app = firebase_admin.initialize_app(cred, {
     'databaseURL':"https://bookstoreproject-8b4f0-default-rtdb.firebaseio.com/"
 })
@@ -16,18 +22,45 @@ refe= db.reference("Datos_del_usuario") #Genera un nodo de informacion diferente
 #Escribir datos en tiempo real
 #[][][][][][][][][][]-- BORRADOR MENU --[][][][][][][][][][][][][][]
 
+#Para validar el correo electrónico
+def validar_correo(correo):
+    while True:
+        caracteres_invalidos = r"^[a-zA-Z0-9]+(\.[a-zA-Z0-9])?@[a-zA-Z]+(\.[a-zA-Z]+)?\.[a-zA-Z]{2,}$"
+        """
+            Se llama la librería 'Re' con la r (raw string), esto evita posibles errores y hace que
+            cada letra se lea de manera literal.
+            Se empieza con '^' para indicar que se lea desde el primer carácter, luego,
+            se abre un [] en donde se indica que son válidos las letras mayúsculas, minúsculas
+            y los dígitos una o más veces (+), opcionalmente (?), puede en otra posición [] haber letras mayúsculas,
+            minúsculas y carácteres separados por UN solo punto (\.)
+            Se busca un @ que esté seguido [] de letras mayúsculas, minúsculas una o más veces.
+            Para el sub dominio opcional (?), se permite nuevamente letras mayúsculas y minúsculas
+            una o más veces. Finalmente, para el dominio principal, se busca que luego del último punto (\.) haya
+            un mínimo de 2 letras mayúsculas o minúsculas.El $ cierra 
+        """
+        if not re.match(caracteres_invalidos, correo): #Se buscan las coincidencias de los carácteres definidos en la variable correo con la librería re
+            print("Error. Asegúrese de digitar un formato válido (ej. usuario@dominio.com)") #Si no está, muestra error, un ejemplo y solicita de nuevo una opción correcta
+            correo = input("Ingrese su correo electrónico: ")#Se vuelve a solicitar el correo
+        else: 
+            return correo #Si coincide, se guarda el valor diitado y termina el ciclo
+
 def ingresoDatos():
     userName=input("Ingrese su UserName: ")
-    examen=int(input("Cuántas veces presento el examen de la Universidad Nacional:  "))
+    examen=int(input("Cuántas veces presento el examen de la Universidad Nacional: "))
     nombre=input("Ingrese su nombre: ")
-    edad=int(input("Ingrese su edad:"))
+    edad=input("Ingrese su edad: ")
+    correo = input("Ingrese su correo")
+    correo = validar_correo(correo)
+    pregunta=input("¿Disfruta de juegos de estrategia como Batalla Naval?: ")
 
     #diccionario de datos
     data={
         "UserName":userName,
         "Intentos_examen":examen,
         "Edad":edad,
-        "Nombre":nombre
+        "Nombre":nombre,
+        "Correo":correo,
+        "Respuesta":pregunta
 
     }
     #cargar los datos
@@ -43,8 +76,10 @@ def mostrarDatos():
             intentosExamen=value.get("Intentos_examen","No hay Intento_examen")
             Nombre=value.get("Nombre","No hay nombre")
             Edad=value.get("Edad","No hay edad")
+            Correo=value.get("Correo","No hay correo")
+            Pregunta=value.get("Respuesta","No hay respuesta")
           
-            print(f" ID: {key}, UserName: {username}, \tIntentos: {intentosExamen}, \tNombre: {Nombre}, \t2Edad: {Edad}")
+            print(f" ID: {key}, UserName: {username}, Intentos: {intentosExamen},Nombre{Nombre},Edad{Edad}")
             
     else:
         print("No hay datos guardados \n") 
@@ -65,13 +100,18 @@ def sobreescribirDatos():
         nuevosIntentos=int(input("Ingrese los intentos que realizó: "))
         nuevonombre=input("Ingrese su nuevo Nombre: ")
         nuevaedad=int(input("Ingrese su nueva Edad: "))
+        nuevoCorreo=input("Ingrese su nuevo correo: ")
+        nuevoCorreo=validar_correo(nuevoCorreo)
+        nuevaPregunta=input("¿Disfruta de juegos de estrategia como Batalla Naval?")
 
         #sobreescibir datos
         nuevosDatos={
             "UserName":nuevoNombre,
             "Intentos_examen":nuevosIntentos,
             "Edad": nuevaedad,
-            "Nombre": nuevonombre
+            "Nombre": nuevonombre,
+            "Correo": nuevoCorreo,
+            "Pregunta": nuevaPregunta
         }
 
         refe.child(subnodo_id).set(nuevosDatos) #refe es el nodo el .child(id del subnodo)se posa sobre ese subnodo; .set() sobreescribe los datos
@@ -87,7 +127,7 @@ def menu():
         print("3. Sobreescribir datos. ")
         print("4. Salir. ")
   
-        opcion=int(input("\nSelecciones la opcion que desea realizar: "))
+        opcion=int(input("\nSeleccione la opcion que desea realizar: "))
 
         if(opcion==1):
             ingresoDatos()
